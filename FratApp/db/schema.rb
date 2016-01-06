1e12afea5060ea95029fb930ba7a70aedaf29127
Jacob Trueb <trueb2@illinois.edu>
2016-01-06 11:08:28 -0600

    added Users controller and User model

--- FratApp/db/schema.rb
+++ FratApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103004943) do
+ActiveRecord::Schema.define(version: 20160106164153) do
 
   create_table "brothers", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20160103004943) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "role"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
